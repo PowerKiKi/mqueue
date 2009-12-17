04712200931987746ce745f75f88c1ac418e847a
PowerKiKi <adrien.crivelli@gmail.com>
2009-12-18 06:42:24 +0900

    FIX even more simplification

--- application/models/StatusMapper.php
+++ application/models/StatusMapper.php
@@ -32,43 +32,16 @@ class Default_Model_StatusMapper extends Default_Model_AbstractMapper
 
     public function find($idUser, $idMovie)
     {
-		$status = new Default_Model_Status();
-        $row = $this->getDbTable()->fetchRow("idUser='$idUser' AND idMovie='$idMovie'");
-
-
-		if ($row == null)
+        $status = $this->getDbTable()->fetchRow("idUser='$idUser' AND idMovie='$idMovie'");
+		
+		if ($status == null)
 		{
+			$status = new Default_Model_Status();
 			$status->setIdUser($idUser);
 			$status->setIdMovie($idMovie);
         }
-		else
-		{
 		
-			$status->setId($row->id)
-				  ->setIdUser($row->idUser)
-				  ->setIdMovie($row->idMovie)
-				  ->setRating($row->rating);
-		}
-				  
 		return $status;
-    }
-
-    public function fetchAll()
-    {
-        $resultSet = $this->getDbTable()->fetchAll();
-        $entries   = array();
-        foreach ($resultSet as $row)
-		{
-            $entry = new Default_Model_Status();
-            $entry->setId($row->id)
-                  ->setIdUser($row->idUser)
-                  ->setIdMovie($row->idMovie)
-                  ->setRating($row->rating)
-                  ;//->setMapper($this);
-            $entries[] = $entry;
-        }
-		
-        return $entries;
     }
 }
 
