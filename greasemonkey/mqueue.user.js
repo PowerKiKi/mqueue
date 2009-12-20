02a96d6e848cd04cef6904679b24be174ecb9b07
PowerKiKi <adrien.crivelli@gmail.com>
2009-12-21 03:41:30 +0900

    FIX optimize greasemonkey script

--- greasemonkey/mqueue.user.js
+++ greasemonkey/mqueue.user.js
@@ -5,28 +5,12 @@
 // ==/UserScript==
 
 
-
-// Les messages sont pr?ormatt? (breakline ?la place de \n)
-var style=".mqueue {padding: 0px; margin: 0px; width: 80px; height: 16px; border: none; overflow: hidden;}";
-var head=document.getElementsByTagName("HEAD")[0];
-var el=window.document.createElement('link');
-el.rel='stylesheet';
-el.type='text/css';
-el.href='data:text/css;charset=utf-8,'+escape(style);
-head.appendChild(el);
-
-
-
-function insertAfter(node, referenceNode)
-{
-	referenceNode.parentNode.insertBefore(node, referenceNode.nextSibling);
-}
 function insertBefore(node, referenceNode)
 {
 	referenceNode.parentNode.insertBefore(node, referenceNode);
 }
 
-var allLinks, thisLink;
+var allLinks;
 allLinks = document.evaluate(
 	'//a[contains(@href, "title/tt")]',
 	document,
@@ -36,18 +20,17 @@ allLinks = document.evaluate(
 
 for (var i = 0; i < allLinks.snapshotLength; i++) 
 {
-	thisLink = allLinks.snapshotItem(i);
+	var thisLink = allLinks.snapshotItem(i);
 	
 	regexp = /www\.imdb\.com\/title\/tt(\d{7})\/$/;
 	if (regexp.test(thisLink.href))
 	{
 		var array = regexp.exec(thisLink.href);
 
-		var link = document.createElement("iframe");
-		link.setAttribute("src", "http://mqueue/status/index/movie/" + array[1]);
-		link.appendChild(document.createTextNode("no iframe text"));
-		insertBefore(link, thisLink);      
-		link.setAttribute("class", 'mqueue');
-
+		var iframe = document.createElement("iframe");
+		iframe.setAttribute("src", "http://mqueue/status/index/movie/" + array[1]);
+		iframe.appendChild(document.createTextNode("no iframe text"));
+		insertBefore(iframe, thisLink);
+		iframe.setAttribute("style", "padding: 0px; margin: 0px; width: 80px; height: 16px; border: none; overflow: hidden;");
 	}
 }
