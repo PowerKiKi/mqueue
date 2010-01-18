7b5e005d5daca727af1fc092ba4b216ec977d126
PowerKiKi <adrien.crivelli@gmail.com>
2010-01-18 23:23:05 +0100

    FIX update status help to match new CSS

--- application/views/helpers/StatusHelp.php
+++ application/views/helpers/StatusHelp.php
@@ -5,14 +5,14 @@ class Default_View_Helper_StatusHelp extends Zend_View_Helper_Abstract
 	public function statusHelp()
 	{
 		$result = '<ul>';
-		$result .= '<li><span class="status current status_need">' . $this->view->translate('Need') . '</span>: ' . $this->view->translate('I want to see this movie') . '</li>';
-		$result .= '<li><span class="status current status_bad">' . $this->view->translate('Bad') . '</span>: ' . $this->view->translate('Boring movie, I wasted my time') . '</li>';
-		$result .= '<li><span class="status current status_ok">' . $this->view->translate('OK') . '</span>: ' . $this->view->translate('Enjoyable movie (most movies)') . '</li>';
-		$result .= '<li><span class="status current status_excellent">' . $this->view->translate('Excellent') . '</span>: ' . $this->view->translate('Really good, I would watch it twice') . '</li>';
-		$result .= '<li><span class="status current status_favorite">' . $this->view->translate('Favorite') . '</span>: ' . $this->view->translate('Incredibly awesome, the kind of movie you must watch many times regurlarly') . '</li>';
+		$result .= '<li><span class="status current status_need"><span>' . Default_Model_Status::$ratings[Default_Model_Status::Need] . '</span></span>: ' . $this->view->translate('I want to see this movie') . '</li>';
+		$result .= '<li><span class="status current status_bad"><span>' . Default_Model_Status::$ratings[Default_Model_Status::Bad] . '</span></span>: ' . $this->view->translate('Boring movie, I wasted my time') . '</li>';
+		$result .= '<li><span class="status current status_ok"><span>' . Default_Model_Status::$ratings[Default_Model_Status::Ok] . '</span></span>: ' . $this->view->translate('Enjoyable movie (most movies)') . '</li>';
+		$result .= '<li><span class="status current status_excellent"><span>' . Default_Model_Status::$ratings[Default_Model_Status::Excellent] . '</span></span>: ' . $this->view->translate('Excellent, I would watch it twice') . '</li>';
+		$result .= '<li><span class="status current status_favorite"><span>' . Default_Model_Status::$ratings[Default_Model_Status::Favorite] . '</span></span>: ' . $this->view->translate('Incredibly awesome, the kind of movie you must watch many times regurlarly') . '</li>';
 		$result .= '</ul>';
+
 		return $result;
 	}
-
 }
 ?>
