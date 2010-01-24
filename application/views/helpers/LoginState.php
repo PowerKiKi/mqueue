8906bf430400a2755b82b5ca1205b35f136a08dc
PowerKiKi <adrien.crivelli@gmail.com>
2010-01-24 14:28:50 +0100

    FIX better menu layout

--- application/views/helpers/LoginState.php
+++ application/views/helpers/LoginState.php
@@ -4,40 +4,38 @@ class Default_View_Helper_LoginState extends Zend_View_Helper_Abstract
 {
 	public function loginState()
 	{
-		$result = '';
+		$result = '<div class="loginState">';
 		$session = new Zend_Session_Namespace();
 		if (isset($session->idUser))
 		{
 			$mapper = new Default_Model_UserMapper();
 			$user = $mapper->find($session->idUser);
 			
+			$result .= $this->view->translate('logged as ');
+			$result .= '<a href="' . $this->view->serverUrl() . $this->view->url(
+				array('controller'=>'user'), 
+				'default', 
+				true) . '">' .  $user->nickname . '</a> ';
+				
 			$result .= '<a href="' . $this->view->serverUrl() . $this->view->url(
 				array('controller'=>'user', 'action' => 'logout'), 
 				'default', 
 				true) . '">' . $this->view->translate('Logout') . '</a> ';
-		
-			$result .= $this->view->translate('logged as ');
-			 $result .= '<a href="' . $this->view->serverUrl() . $this->view->url(
-				array('controller'=>'user'), 
-				'default', 
-				true) . '">' .  $user->nickname . '</a> ';
-		;
 		}
 		else
 		{
-			$result .= '<a href="' . $this->view->serverUrl() . $this->view->url(
-				array('controller'=>'user', 'action' => 'login'), 
-				'default', 
-				true) . '">' . $this->view->translate('Login') . '</a>';
-		
 			$result .= ' <a href="' . $this->view->serverUrl() . $this->view->url(
 				array('controller'=>'user', 'action' => 'new'), 
 				'default', 
 				true) . '">' . $this->view->translate('Subscribe') . '</a> ';
-			$result .= $this->view->translate('not logged in');
+				
+			$result .= '<a href="' . $this->view->serverUrl() . $this->view->url(
+				array('controller'=>'user', 'action' => 'login'), 
+				'default', 
+				true) . '">' . $this->view->translate('Login') . '</a>';
 		}
 		
-		return $result;
+		return $result . '</div>';
 	}
 
 }
