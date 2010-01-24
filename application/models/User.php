d810b01cd294892480c811c4873c5c13e2d4f788
PowerKiKi <adrien.crivelli@gmail.com>
2010-01-24 01:30:51 +0100

    ADD first attempt for styling

--- application/models/User.php
+++ application/models/User.php
@@ -2,6 +2,11 @@
 
 class Default_Model_User extends Default_Model_AbstractModel
 {
+	public static function getCurrent()
+	{
+		
+	}
+	
 	public function getStatistics()
 	{
 		$mapper = new Default_Model_StatusMapper();
