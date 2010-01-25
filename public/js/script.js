c44b018ed4b65116f92ad2710615b2dfccc22bab
PowerKiKi <adrien.crivelli@gmail.com>
2010-01-25 08:30:54 +0100

    ADD support for main movie page on IMDB (status is next to title)

--- public/js/script.js
+++ public/js/script.js
@@ -21,9 +21,10 @@ function scanIMDB(server)
 {
 	var query = '';
 	var list = new Array();
-	$("a[href*='title/tt']").each(function()
+	
+	// Find every references to any movies
+	$("a[href*='title/tt'], link[rel='canonical']").each(function()
 	{
-
 		regexp = /www\.imdb\.com\/title\/tt(\d{7})\/$/;
 		if (regexp.test(this.href))
 		{
@@ -42,14 +43,18 @@ function scanIMDB(server)
 	if (query == '')
 		return;
 	
+	// Get statuses from server
 	var url = server + "status/list/movies/" + query + "?format=json&jsoncallback=?";
 	$.getJSON(url, function(data)
 	{
 		$.each(data.status, function(id, status)
 		{
-			q = "a[href*='/title/tt" + id.split('_')[0] + "']";
-			
-			$(q).before(status);
+			// Add status beside main title if on the main page of movie
+			if ($("link[rel='canonical'][href*=" + id.split('_')[0] + "]").length != 0)
+				$("#tn15title>h1").before(status);
+			// Add status on every links concerning that movie
+			else
+				$("a[href*='/title/tt" + id.split('_')[0] + "']").before(status);
 		});
 		
 		scanStatus();
