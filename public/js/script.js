d51ad93c2d3969141726eddc6337a131a7d1490e
PowerKiKi <adrien.crivelli@gmail.com>
2010-01-28 22:52:34 +0100

    ADD support for virtually unlimited links on the same page (not more URL length limit) FIX huge performance improvement on remote side FIX bug which binded many times the ajax query, resulting in very slow responses

--- public/js/script.js
+++ public/js/script.js
@@ -1,28 +1,44 @@
-function scanStatus()
+/**
+ * JSONp function to set and retrieve new status
+ * @return
+ */
+function setStatus()
 {
-	$('.status').each(function()
+	$.getJSON(this.href + '?format=json&jsoncallback=?', function(data)
 	{
-		$(this).click(function()
+		$('.status_links_' + data.id).each(function()
 		{
-			$.getJSON(this.href + '?format=json&jsoncallback=?', function(data)
-			{
-				$('.status_links_' + data.id).each(function()
-				{
-					$(this).replaceWith(data.status);
-				})
-				scanStatus();
-			})		
-			return false;
+			$(this).replaceWith(data.status);
 		})
-	})
+		
+		$('.status_links_' + data.id + ' .status').click(setStatus);
+	});		
+	return false;
 }
 
+/**
+ * 	Bind ajax on all status links
+ * @return
+ */
+function bindStatus()
+{
+	$('.status').click(setStatus);
+}
+
+/**
+ * Scan the current page for IMDB links and add appropriate icons
+ * @param server
+ * @return
+ */
 function scanIMDB(server)
 {
-	var query = '';
+	const maxPerQuery = 400;	
 	var list = new Array();
+	var queries = new Array();
 	
 	// Find every references to any movies
+	var query = '';
+	var i = 0;
 	$("a[href*='title/tt'], link[rel='canonical']").each(function()
 	{
 		regexp = /www\.imdb\.com\/title\/tt(\d{7})\/$/;
@@ -36,32 +52,44 @@ function scanIMDB(server)
 				query += id + ',';
 			}
 			list[id].push(this);
+			
+			if (i++ > maxPerQuery)
+			{
+				queries.push(query);
+				query = '';
+				i = 0;
+			}
 		}
-		
 	})
 	
-	if (query == '')
+	if (i != 0)
+		queries.push(query);
+	
+	if (queries.length == 0)
 		return;
 	
-	// Get statuses from server
-	var url = server + "status/list/movies/" + query + "?format=json&jsoncallback=?";
-	$.getJSON(url, function(data)
+	// Get statuses from server per bunch of queries
+	$.each(queries, function(x, query)
 	{
-		$.each(data.status, function(id, status)
+		var url = server + "status/list/movies/" + query + "?format=json&jsoncallback=?";
+		$.getJSON(url, function(data)
 		{
-			// Add status beside main title if on the main page of movie
-			if ($("link[rel='canonical'][href*=" + id.split('_')[0] + "]").length != 0)
-				$("#tn15title>h1").before(status);
-			// Add status on every links concerning that movie
-			else
-				$("a[href*='/title/tt" + id.split('_')[0] + "']").before(status);
+			$.each(data.status, function(id, status)
+			{
+				// Add status beside main title if on the main page of movie
+				if ($("link[rel='canonical'][href*=" + id.split('_')[0] + "]").length != 0)
+					$("#tn15title>h1").before(status);
+				// Add status on every links concerning that movie
+				else
+					$("a[href*='/title/tt" + id.split('_')[0] + "']").before(status);
+			});
+			
+			bindStatus();
 		});
-		
-		scanStatus();
 	});
 }
 
 $(document).ready(function()
 {
-	scanStatus();
-})
+	bindStatus();
+});
