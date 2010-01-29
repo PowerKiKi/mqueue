bc92b1ce39bc9c9f6ffc562109fe0914ae56d180
PowerKiKi <adrien.crivelli@gmail.com>
2010-01-29 23:43:19 +0100

    FIX prevent remove status when using others icon (and cannot see the actual result). So to remove a status, we must use our own icon

--- application/views/helpers/StatusLinks.php
+++ application/views/helpers/StatusLinks.php
@@ -5,13 +5,14 @@ class Default_View_Helper_StatusLinks extends Zend_View_Helper_Abstract
 	public function statusLinks(Default_Model_Status $status)
 	{
 		$result = '<div class="status_links status_links_' . $status->getUniqueId() .'">';
+		$user = Default_Model_User::getCurrent();
 		foreach (Default_Model_Status::$ratings as $val => $name)
 		{
 			$class = $val . ($status->rating == $val ? ' current' : '');
 			$url = $this->view->serverUrl() . $this->view->url(array(
 														'controller' => 'status',
 														'movie' => $status->idMovie,
-														'rating' => ($val == $status->rating) ? 0 : $val
+														'rating' => ($val == $status->rating && $user && $user->id == $status->idUser) ? 0 : $val
 			),
 													'default', 
 			true);
