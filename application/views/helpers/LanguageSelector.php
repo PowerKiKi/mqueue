a52056c912f56ec08b0494548cf19cb01e322939
PowerKiKi <adrien.crivelli@gmail.com>
2010-02-08 15:23:58 +0100

    DEL flags for languages. should not use flags for languages and was ugly anyway

--- application/views/helpers/LanguageSelector.php
+++ application/views/helpers/LanguageSelector.php
@@ -5,15 +5,15 @@ class Default_View_Helper_LanguageSelector extends Zend_View_Helper_Abstract
 	public function languageSelector()
 	{
 		$languages = array(
-			'en' => _tr('English'),
-			'ko' => _tr('Korean'),
-			'fr' => _tr('French'),
+			'en' => 'English',
+			'ko' => '한국어',
+			'fr' => 'Français',
 			);
 		
 		$result = '<div class="language_selector">';
 		foreach ($languages as $val => $name)
 		{
-			$result .= '<a class="language language_' . $val . '" href="?lang=' . $val . '" title="' . $name . '"><span>' . $name . '</span></a>';
+			$result .= '<a class="language language_' . $val . '" href="?lang=' . $val . '" title="' . $name . '"><span>' . $name . '</span></a> ';
 		}
 		$result .= '</div>';
 
