fa75864397dead6ae10f6da898ae9bec03e0b44a
PowerKiKi <adrien.crivelli@gmail.com>
2010-10-02 23:30:42 +0200

    FIX escape issue with user's name in sorter tooltip (title)

--- application/views/helpers/Sort.php
+++ application/views/helpers/Sort.php
@@ -8,7 +8,7 @@ class Default_View_Helper_Sort extends Zend_View_Helper_Abstract
 		if (!in_array($sortOrder, $orders)) $sortOrder = reset($orders);
 		
 		$url = $this->view->url(array('sort' => $column, 'sortOrder' => $orders[$sortOrder]));
-		$result = '<a class="sort ' . ($column == $sort ? $sortOrder : '') . '" title="' . $this->view->escape($this->view->translate('Sort by "%s"', array($label))) . '" href="' . $url . '">' . $label . '</a>';
+		$result = '<a class="sort ' . ($column == $sort ? $sortOrder : '') . '" title="' . $this->view->escape($this->view->translate('Sort by "%s"', array($label))) . '" href="' . $url . '">' . $this->view->escape($label) . '</a>';
 
 		return $result;
 	}
