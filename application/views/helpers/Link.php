be939741a9e53225e2b513b04590b860e1c231a4
PowerKiKi <adrien.crivelli@gmail.com>
2010-11-20 18:23:37 +0100

    FIX only one IMDb link on movie page (instead of two before which caused double insertions of icons by other mQueue instance)

--- application/views/helpers/Link.php
+++ application/views/helpers/Link.php
@@ -2,15 +2,10 @@
 
 class Default_View_Helper_Link extends Zend_View_Helper_Abstract
 {
-	public function link(Default_Model_Movie $movie, $displayUrl = false)
+	public function link(Default_Model_Movie $movie, $showUrl = false)
 	{
 		$url =  $movie->getImdbUrl();
-		$result = '<a class="imdb" title="' . $url . '" href="' . $url . '"><span>' . $this->view->translate('IMDb') . '</span></a>';
-		
-		if ($displayUrl)
-		{
-			$result .= ' <a href="' . $url . '">' . $url . '</a>';
-		}
+		$result = '<a class="imdb' .($showUrl ? '' : ' hideUrl') . '" title="' . $url . '" href="' . $url . '"><span>&nbsp;' . $url . '</span></a>';
 		
 		return $result;
 	}
