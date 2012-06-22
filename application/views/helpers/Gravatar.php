a42755ee57fcb1f690c9760580c38614276ec46d
Adrien Crivelli <adrien.crivelli@ecodev.ch>
2012-06-23 02:45:00 +0900

    HTTPS compatibility
    Always use secured gravatar to avoid mixed content warnings.
    Exclude injection of icons on our website for both protocol (http and https)

--- application/views/helpers/Gravatar.php
+++ application/views/helpers/Gravatar.php
@@ -3,20 +3,12 @@
 class Default_View_Helper_Gravatar extends Zend_View_Helper_Abstract
 {
 	/**
-	 * Return an image for a user on gravatar
+	 * Return a span with CSS class for a user on gravatar
 	 * @param Default_Model_User $user
 	 * @param boolean $small
 	 */
 	public function gravatar(Default_Model_User $user, $small = true)
 	{
-		$url = 'http://www.gravatar.com/avatar/' . md5(trim(strtolower($user->email))) . '.jpg?';
-		$url .= 'default=identicon';
-
-		if ($small)
-			$url .= ('&amp;size=16');
-		else
-			$url .= ('&amp;size=96');
-
 		$result = '<span class="gravatar user_' . $user->id . ' ' . ($small ? 'small' : 'big') . '"></span>';
 
 		return $result;
