64e19d61ef5f74df2e12dab720dfdb161ecc28d5
Adrien Crivelli <adrien.crivelli@ecodev.ch>
2013-01-20 17:20:33 +0100

    Backport Error controller from Zend 1.12.0

--- application/controllers/ErrorController.php
+++ application/controllers/ErrorController.php
@@ -7,25 +7,32 @@ class ErrorController extends Zend_Controller_Action
     {
         $errors = $this->_getParam('error_handler');
         
+        if (!$errors || !$errors instanceof ArrayObject) {
+            $this->view->message = 'You have reached the error page';
+            return;
+        }
+        
         switch ($errors->type) {
             case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_ROUTE:
             case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_CONTROLLER:
             case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_ACTION:
-        
                 // 404 error -- controller or action not found
                 $this->getResponse()->setHttpResponseCode(404);
+                $priority = Zend_Log::NOTICE;
                 $this->view->message = 'Page not found';
                 break;
             default:
                 // application error
                 $this->getResponse()->setHttpResponseCode(500);
+                $priority = Zend_Log::CRIT;
                 $this->view->message = 'Application error';
                 break;
         }
         
         // Log exception, if logger available
         if ($log = $this->getLog()) {
-            $log->crit($this->view->message, $errors->exception);
+            $log->log($this->view->message, $priority, $errors->exception);
+            $log->log('Request Parameters', $priority, $errors->request->getParams());
         }
         
         // conditionally display exceptions
@@ -39,7 +46,7 @@ class ErrorController extends Zend_Controller_Action
     public function getLog()
     {
         $bootstrap = $this->getInvokeArg('bootstrap');
-        if (!$bootstrap->hasPluginResource('Log')) {
+        if (!$bootstrap->hasResource('Log')) {
             return false;
         }
         $log = $bootstrap->getResource('Log');
