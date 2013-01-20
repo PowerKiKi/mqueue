d35be36a3ff63e212cf560cb25ca85411e8d8fa4
Adrien Crivelli <adrien.crivelli@ecodev.ch>
2013-01-20 18:42:56 +0100

    Introduce Google Closure Compiler
    Optimize HTTP requests and file size via minification and concatenation

---
+++ scripts/build.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+echo "Compiling JavaScript..."
+cd public/js
+mkdir -p min
+for file in *.js ; do
+	
+	# Discard warnings for third party code
+	if [[ $file =~ jquery|jcarousellite ]]
+	then
+		thirdparty="--third_party --warning_level QUIET"
+	else
+		thirdparty=""
+	fi
+
+	echo "$file"
+	java -jar ../../library/closure-compiler/compiler.jar --compilation_level SIMPLE_OPTIMIZATIONS  --js "$file" --js_output_file "min/$file" $thirdparty
+done
+
+
+echo "Concatenate JavaScript..."
+cd min/
+
+# CAUTION: This must be the exact same files in reverse order than in application/layout/layout.phtml
+cat jquery-1.8.3.js \
+both.js \
+local.js \
+jquery.timeago.js \
+> application.js
