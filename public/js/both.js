1ecd8292a9d6241a9fc99a75902346177ced0c30
Adrien Crivelli <adrien.crivelli@gmail.com>
2019-11-09 22:51:45 +0100

    Code style

--- public/js/both.js
+++ public/js/both.js
@@ -1,19 +1,16 @@
 /*global $, document */
 
-var mqueue = (function() {
+const mqueue = (function () {
 
     /**
      * JSONp function to set and retrieve new status
      * @return
      */
-    function setStatus()
-    {
-        var parent = $(this).parent();
+    function setStatus() {
+        const parent = $(this).parent();
         parent.addClass('loading');
-        $.getJSON(this.href + '?format=json&jsoncallback=?', function(data)
-        {
-            $('.mqueue_status_links_' + data.id).each(function()
-            {
+        $.getJSON(this.href + '?format=json&jsoncallback=?', function (data) {
+            $('.mqueue_status_links_' + data.id).each(function () {
                 $(this).replaceWith(data.status);
             });
 
@@ -27,9 +24,7 @@ var mqueue = (function() {
      * Bind ajax on all status links
      * @return
      */
-    //var bindStatus = function ()
-    function bindStatus()
-    {
+    function bindStatus() {
         $('a.mqueue_status').click(setStatus);
     }
 
@@ -39,36 +34,31 @@ var mqueue = (function() {
      * @param node
      * @return
      */
-    function injectStatus(server, node)
-    {
-        var maxPerQuery = 400;
-        var list = [];
-        var queries = [];
-        var query = '';
-        var i = 0;
+    function injectStatus(server, node) {
+        const maxPerQuery = 400;
+        const list = [];
+        const queries = [];
+        let query = '';
+        let i = 0;
 
         // Find every references to any movies, within the node, or the node itself
-        var selector = "a[href*='title/tt'], link[rel='canonical']";
-        var matches = $(selector, node);
+        const selector = 'a[href*=\'title/tt\'], link[rel=\'canonical\']';
+        const matches = $(selector, node);
         if ($(node).is(selector))
             matches.push(node[0]);
 
-        $.each(matches, function()
-        {
-            var regexp = /imdb\.(com|de|es|fr|it|pt)\/title\/tt(\d{7,})/;
-            if (regexp.test(this.href))
-            {
-                var array = regexp.exec(this.href);
-                var id = array[2];
-                if (list[id] === undefined)
-                {
+        $.each(matches, function () {
+            const regexp = /imdb\.(com|de|es|fr|it|pt)\/title\/tt(\d{7,})/;
+            if (regexp.test(this.href)) {
+                const array = regexp.exec(this.href);
+                const id = array[2];
+                if (list[id] === undefined) {
                     list[id] = [];
                     query += id + ',';
                 }
                 list[id].push(this);
 
-                if (i++ > maxPerQuery)
-                {
+                if (i++ > maxPerQuery) {
                     queries.push(query);
                     query = '';
                     i = 0;
@@ -76,34 +66,27 @@ var mqueue = (function() {
             }
         });
 
-        if (i !== 0)
-        {
+        if (i !== 0) {
             queries.push(query);
         }
 
-        if (queries.length === 0)
-        {
+        if (queries.length === 0) {
             return;
         }
 
         // Get statuses from server per bunch of queries
-        $.each(queries, function(x, query)
-        {
-            var url = server + "status/list/movies/" + query + "?format=json&jsoncallback=?";
-            $.getJSON(url, function(data)
-            {
-                $.each(data.status, function(id, status)
-                {
+        $.each(queries, function (x, query) {
+            const url = server + 'status/list/movies/' + query + '?format=json&jsoncallback=?';
+            $.getJSON(url, function (data) {
+                $.each(data.status, function (id, status) {
                     // Add status beside main title if on the main page of movie
-                    if ($("link[rel='canonical'][href*='" + id.split('_')[0] + "']").length !== 0)
-                    {
+                    if ($('link[rel=\'canonical\'][href*=\'' + id.split('_')[0] + '\']').length !== 0) {
                         // New IMDb version, add under the main title in the subtext line
-                        $(".title_wrapper .subtext", node).append('<span class="ghost">|</span>' + status);
+                        $('.title_wrapper .subtext', node).append('<span class="ghost">|</span>' + status);
                     }
                     // Add status on every links concerning that movie
-                    else
-                    {
-                        var selector = "a[href*='/title/tt" + id.split('_')[0] + "']";
+                    else {
+                        const selector = 'a[href*=\'/title/tt' + id.split('_')[0] + '\']';
                         $(selector, node).after(status);
                         $(node).filter(selector).after(status);
                     }
@@ -118,30 +101,29 @@ var mqueue = (function() {
      * Monitor any links refering to IMDb, existing now or in future
      * @param server
      **/
-    function monitorLinks(server)
-    {
-        var timeoutId;
-        var insertedNodes = [];
+    function monitorLinks(server) {
+        let timeoutId;
+        let insertedNodes = [];
 
         injectStatus(server, $(document));
-        $('body').bind('DOMNodeInserted', function(e) {
+        $('body').bind('DOMNodeInserted', function (e) {
             insertedNodes.push(e.target);
 
             // Cancel previous timeout if any
-            if (typeof timeoutId == "number") {
+            if (typeof timeoutId == 'number') {
                 window.clearTimeout(timeoutId);
                 delete timeoutId;
             }
 
             // Set short timeout to actually injectStatus (several nodes at once)
-            timeoutId = window.setTimeout(function() {
+            timeoutId = window.setTimeout(function () {
 
                 // Filter inserted nodes to keep only nodes without status links (to avoid duplicated injection)
-                var nodesWitoutStatus = [];
-                $.each(insertedNodes, function(x, node) {
+                const nodesWitoutStatus = [];
+                $.each(insertedNodes, function (x, node) {
 
-                    var selector = ".mqueue_status_links";
-                    var statusExist = $(e.target).is(selector) || $(selector, e.target).length;
+                    const selector = '.mqueue_status_links';
+                    const statusExist = $(e.target).is(selector) || $(selector, e.target).length;
                     if (!statusExist) {
                         nodesWitoutStatus.push(node);
                     }
@@ -158,12 +140,11 @@ var mqueue = (function() {
      */
     return {
         bindStatus: bindStatus,
-        monitorLinks: monitorLinks
+        monitorLinks: monitorLinks,
     };
 
 }());
 
-$(document).ready(function()
-{
+$(document).ready(function () {
     mqueue.bindStatus();
 });
