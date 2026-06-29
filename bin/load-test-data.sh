80efc24c183cd0a45ab6e1228b9a64bd2c242923
Adrien Crivelli <adrien.crivelli@gmail.com>
2026-06-29 09:10:39 +0200

    Can load test data on GitHub Actions

--- bin/load-test-data.sh
+++ bin/load-test-data.sh
@@ -2,6 +2,9 @@
 
 set -xe
 
-sudo mariadb mqueue -e 'DROP DATABASE IF EXISTS mqueue; CREATE DATABASE mqueue;'
+# On GitHub Actions we want to connect via tcp, but locally we prefer socket
+HOST=$([ -n "$CI" ] && echo '127.0.0.1' || echo 'localhost')
+
+sudo mariadb -h $HOST -e 'DROP DATABASE IF EXISTS mqueue; CREATE DATABASE mqueue;'
 ./bin/doctrine migrations:migrate --no-interaction
 cat ./tests/data/fixture.sql | sudo mariadb mqueue
