273bde2a1e388eef6d7806784d3989d2fcaa0812
Adrien Crivelli <adrien.crivelli@gmail.com>
2026-06-29 09:17:36 +0200

    Can load test data on GitHub Actions (bis)

--- bin/load-test-data.sh
+++ bin/load-test-data.sh
@@ -7,4 +7,4 @@ HOST=$([ -n "$CI" ] && echo '127.0.0.1' || echo 'localhost')
 
 sudo mariadb -h $HOST -e 'DROP DATABASE IF EXISTS mqueue; CREATE DATABASE mqueue;'
 ./bin/doctrine migrations:migrate --no-interaction
-cat ./tests/data/fixture.sql | sudo mariadb mqueue
+cat ./tests/data/fixture.sql | sudo mariadb mqueue -h $HOST
