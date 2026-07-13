73712526e385a8a2360c846c5a9b51b7e13a092c
Adrien Crivelli <adrien.crivelli@gmail.com>
2026-07-13 16:08:29 +0900

    year instead of start_year

--- tests/data/fixture.sql
+++ tests/data/fixture.sql
@@ -11,7 +11,7 @@ INSERT INTO user (id, date_update, email, password, nickname) VALUES
 (1002, '2020-01-02', 'user2@example.com', SHA1('user2'), 'user2'),
 (1003, '2020-01-03', 'user3@example.com', SHA1('user3'), 'user3');
 
-INSERT INTO movie (id, date_update, title, start_year, date_search, search_count, identity, quality, score, source) VALUES
+INSERT INTO movie (id, date_update, title, year, date_search, search_count, identity, quality, score, source) VALUES
 (96446, '2020-02-01', 'Willow', 1988, NULL, 0, 0, 0, 0, NULL),
 (103064, '2020-02-02', 'Terminator 2', 1991, '2020-03-01', 2, 3, 4, 5, 'some link'),
 (28650488, '2020-02-03', 'The Super Mario Galaxy Movie', 2026, NULL, 0, 0, 0, 0, NULL);
